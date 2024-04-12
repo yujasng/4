f3493c808cdba7a3384a1f29acb71b62a174ac5b
michelle3555 <michelle3555@snu.ac.kr>
2024-04-12 09:37:14 +0900

    f

--- calc.py
+++ calc.py
@@ -1,2 +1,17 @@
 def main():
-  print("We are subteam C")
+  print("This is calculator")
+  
+  
+def add(x,y):
+  x = int(input())
+  y = int(input())
+  return x+y
+
+def multiply(x,y):
+  x = int(input())
+  y = int(input())
+  return x*y
+  
+
+if __name__ == "__main__":
+  main()
